33a0e0e00efb4630bac55e7eb0ccaf2c2391d155
Andrew Wittrock <andy@wittrock.us>
2025-11-23 23:57:01 -0600

    Set priority on init migration

--- migrations/00-init/index.ts
+++ migrations/00-init/index.ts
@@ -6,37 +6,37 @@ import { arcsTable, charactersTable, pageCharactersTable, pageCommentaryTable, p
 const db = database();
 
 const permissions = [
-    {name: "comicPage.create", description: "Can create comic pages"},
-    {name: "comicPage.view", description: "Can view comic pages"},
-    {name: "comicPage.update", description: "Can edit comic pages"},
-    {name: "comicPage.delete", description: "Can delete comic pages"},
+    { name: "comicPage.create", description: "Can create comic pages" },
+    { name: "comicPage.view", description: "Can view comic pages" },
+    { name: "comicPage.update", description: "Can edit comic pages" },
+    { name: "comicPage.delete", description: "Can delete comic pages" },
 
-    {name: "comicArc.create", description: "Can create comic arcs"},
-    {name: "comicArc.view", description: "Can view comic arcs"},
-    {name: "comicArc.update", description: "Can edit comic arcs"},
-    {name: "comicArc.delete", description: "Can delete comic arcs"},
+    { name: "comicArc.create", description: "Can create comic arcs" },
+    { name: "comicArc.view", description: "Can view comic arcs" },
+    { name: "comicArc.update", description: "Can edit comic arcs" },
+    { name: "comicArc.delete", description: "Can delete comic arcs" },
 
-    {name: "comicCharacter.create", description: "Can create comic characters"},
-    {name: "comicCharacter.view", description: "Can view comic characters"},
-    {name: "comicCharacter.update", description: "Can edit comic characters"},
-    {name: "comicCharacter.delete", description: "Can delete comic characters"},
+    { name: "comicCharacter.create", description: "Can create comic characters" },
+    { name: "comicCharacter.view", description: "Can view comic characters" },
+    { name: "comicCharacter.update", description: "Can edit comic characters" },
+    { name: "comicCharacter.delete", description: "Can delete comic characters" },
 
-    {name: "comicCommentary.create", description: "Can create comic page commentary"},
-    {name: "comicCommentary.view", description: "Can view comic page commentary"},
-    {name: "comicCommentary.update", description: "Can edit comic page commentary"},
-    {name: "comicCommentary.delete", description: "Can delete comic page commentary"},
+    { name: "comicCommentary.create", description: "Can create comic page commentary" },
+    { name: "comicCommentary.view", description: "Can view comic page commentary" },
+    { name: "comicCommentary.update", description: "Can edit comic page commentary" },
+    { name: "comicCommentary.delete", description: "Can delete comic page commentary" },
 ];
 
 const rolePermissions = [
-    ...permissions                                      .map(p => ({roleName: "SuperUser", permissionName: p.name})),
-    ...permissions.filter(p => p.name.endsWith(".view")).map(p => ({roleName: "Public",    permissionName: p.name})),
+    ...permissions.map(p => ({ roleName: "SuperUser", permissionName: p.name })),
+    ...permissions.filter(p => p.name.endsWith(".view")).map(p => ({ roleName: "Public", permissionName: p.name })),
 ];
 
-export const init:IMigration = {
+export const init: IMigration = {
     name: "init-comic-module",
     module: "comic",
     description: "Initial data for comic module",
-    order: 0,
+    order: 1,
     down: () => db.schema
         .dropTableIfExists("comicPageCharacters")
         .dropTableIfExists("comicPageCommentaries")
