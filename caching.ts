e24b730b0287567431d204b86b5a68999d0d5b64
Andrew Wittrock <andy@wittrock.us>
2026-02-19 12:46:57 -0600

    Remove cache types

--- caching.ts
+++ caching.ts
@@ -1,6 +1,4 @@
-import { IBehavior } from "../core/cloudfront";
-
-export const comicCaching:IBehavior[] = [
+export const comicCaching = [
     {precedence: 1, pathPattern: "/character*", cache: true },
     {precedence: 2, pathPattern: "/page*",   cache: true },
     {precedence: 3, pathPattern: "/arc*",cache: true },
