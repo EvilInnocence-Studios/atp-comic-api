e366e6cca8fff094bd2901e117459a8dc4c7df78
Andrew Wittrock <andy@wittrock.us>
2026-05-17 14:16:15 -0500

    Add FieldRegistry to only allow some fields to be updated via the API

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { FieldRegistry } from "@core/express/util";
 import { init } from "../comic/migrations/00-init";
 import { migration as addArcCharacters } from "../comic/migrations/01-addArcCharacters";
 
@@ -5,3 +6,32 @@ export { apiConfig } from "./endpoints";
 
 export const migrations = [init, addArcCharacters];
 export const setupMigrations = [init];
+
+FieldRegistry.register(
+    "comicArcs",
+    ["name", "url", "parentId", "sortOrder", "enabled", "isVerticalScroll", "thumbnailUrl", "bannerUrl", "summary", "transcript", "postDate"]
+);
+FieldRegistry.register(
+    "comicCharacters",
+    ["name", "thumbnailId", "mainImageId", "enabled", "showDetails", "sortOrder", "bio"]
+);
+FieldRegistry.register(
+    "comicCharacterAttributes",
+    ["name", "value", "sortOrder"]
+);
+FieldRegistry.register(
+    "comicCharacterMedia",
+    ["url", "caption", "order"]
+);
+FieldRegistry.register(
+    "comicPages",
+    ["name", "url", "arcId", "sortOrder", "imageUrl", "transcript", "enabled", "postDate"]
+);
+FieldRegistry.register(
+    "comicPageCharacters",
+    []
+);
+FieldRegistry.register(
+    "comicPageCommentaries",
+    ["title", "text", "sortOrder"]
+);
